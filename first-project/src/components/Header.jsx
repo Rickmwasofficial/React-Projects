fbb6c922b16e2220beda1626c6078c291ecb17ec
Rickmwasofficial <rickmwasofficial@gmail.com>
2024-12-12 15:11:51 +0300

    Projects for learning react

---
+++ first-project/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="/react-1-logo-svg-vector.svg" alt="React Logo" />
+            <h1>ReactFun</h1>
+        </header>
+    )
+}
---
+++ first-project/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+export default function MainContent() {
+    return (
+        <>
+            <h2>Fun Facts about React</h2>
+            <ul className="facts">
+                <li>Owned by Meta</li>
+                <li>Has well about 100K stars on Github</li>
+                <li>Was created in 2013</li>
+                <li>Can easily get you a job</li>
+            </ul>
+        </>
+    )
+}
