04a5da94d4e13943cb39f92621aa1156d2fdee8c
Rickmwasofficial <rickmwasofficial@gmail.com>
2024-12-12 15:35:23 +0300

    initial setup

---
+++ second-project/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="topHeader">
+            <img src="/undraw_connected_world_wuay.svg" width='40px' alt="" />
+            <h1>Maverick Travel Journal</h1>
+        </header>
+    )
+}
