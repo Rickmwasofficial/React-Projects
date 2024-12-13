3fc7a9266b7a3e926f14c28b437fb472cd3bdbb1
Rickmwasofficial <rickmwasofficial@gmail.com>
2024-12-13 11:07:56 +0300

    Implemented props to load data dynamically

--- second-project/src/App.jsx
+++ second-project/src/App.jsx
@@ -1,16 +1,22 @@
 import Header from './components/Header.jsx'
 import Entry from './components/Entry'
 import './App.css'
+import data from './data.js'
 
 export default function App() {
+  const entryElements = data.map((entry) => {
+      return (
+        <Entry 
+          key= {entry.id}
+          {...entry}
+        />
+      )
+  })
   return (
     <>
       <Header />
       <div className="travel-data">
-        <Entry />
-        <Entry />
-        <Entry />
-        <Entry />
+        {entryElements}
       </div>
     </>
   )
--- second-project/src/components/Entry.jsx
+++ second-project/src/components/Entry.jsx
@@ -1,23 +1,24 @@
-export default function Entry() {
+export default function Entry(props) {
+    console.log(props)
     return (
         <div className="entry-item">
             <div className="img">
-                <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="" srcset="" />
+                <img src={props.img.src} alt="" srcSet={props.img.srcset} />
             </div>
             <div className="details">
                 <section className='top'>
                     <div>
-                        <img src="/placeholder.png" width='20px' alt="" srcset="" />
-                        <p>JAPAN</p>
+                        <img src="/placeholder.png" width='20px' alt="" srcSet="" />
+                        <p>{props.country}</p>
                     </div>
-                    <a href='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu'><p>View on Google Maps</p></a>
+                    <a href={props.googleMapsLink}><p>View on Google Maps</p></a>
                 </section>
                 <section className="loc">
-                    <h1>Mount Fuji</h1>
+                    <h1>{props.title}</h1>
                 </section>
                 <section className="bottom">
-                    <p className="date">12 Jan, 2021 - 24 Jan 2021</p>
-                    <p className="desc">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                    <p className="date">{props.dates}</p>
+                    <p className="desc">{props.text}</p>
                 </section>
             </div>
         </div>
